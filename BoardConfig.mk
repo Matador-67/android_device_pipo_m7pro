670e3e555a507c49a4a37e7c4358b14b940ca72d
Matador-67 <Matador-67@users.noreply.github.com>
2015-04-13 21:19:00 +0200

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,14 +1,14 @@
 USE_CAMERA_STUB := true
 
 # inherit from the proprietary version
--include vendor/pipo/m7pro/BoardConfigVendor.mk
+-include vendor/pipo/m9pro/BoardConfigVendor.mk
 
 TARGET_ARCH := arm
 TARGET_NO_BOOTLOADER := true
 
 
 TARGET_BOARD_PLATFORM := unknown
-TARGET_BOARD_HARDWARE := m7pro
+TARGET_BOARD_HARDWARE := m9pro
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
 TARGET_ARCH_VARIANT := armv7-a-neon
